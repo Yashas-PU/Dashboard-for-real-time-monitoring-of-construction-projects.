f81c582010083fad222b9c565c367ace0abfceaf
YASHAS R GOWDA <yrg123123@gmail.com>
2025-05-08 12:01:56 +0530

    Delete userService.js

--- userService.js
+++
@@ -1,9 +0,0 @@
-const { User } = require("../models");
-
-exports.getUsers = async () => {
-    return await User.findAll();
-};
-
-exports.createUser = async (data) => {
-    return await User.create(data);
-};
