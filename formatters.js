a56490d1330c612538033fda2af1af5b2cbdc973
YASHAS R GOWDA <yrg123123@gmail.com>
2025-05-08 16:29:33 +0530

    Delete formatters.js

--- formatters.js
+++
@@ -1,7 +0,0 @@
-exports.formatDate = (date) => {
-  return new Date(date).toISOString().split("T")[0];
-};
-
-exports.formatCurrency = (amount) => {
-  return new Intl.NumberFormat("en-IN", { style: "currency", currency: "INR" }).format(amount);
-};
