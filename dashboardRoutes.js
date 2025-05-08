6f24a47c16198f36690b1501f9023d9b2be40071
YASHAS R GOWDA <yrg123123@gmail.com>
2025-05-08 12:01:42 +0530

    Delete dashboardRoutes.js

--- dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getDashboardStats } = require("../controllers/dashboardController");
-
-const router = express.Router();
-
-router.get("/dashboard", getDashboardStats);
-
-module.exports = router;
